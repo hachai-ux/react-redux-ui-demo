9005da5a800284222db031de98a7fe3ac3022545
Hac Hai Pham <hpham.ux@gmail.com>
2022-03-24 15:47:38 +0100

    Add selector for todo list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from 'react'
+
+import Header from './components/Header'
+import TodoList from './components/TodoList'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+      <div className="App">
+        <nav>
+          <section>
+            <h1>Redux Fundamentals Example</h1>
+          </section>
+        </nav>
+        <main>
+          <section className="medium-container">
+            <h2>Todos</h2>
+            <div className="todoapp">
+              <Header />
+              <TodoList />
+              <Footer />
+            </div>
+          </section>
+        </main>
+      </div>
+  )
 }
 
-export default App;
+export default App
